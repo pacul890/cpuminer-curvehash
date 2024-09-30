d66ec82ccfe0d435572ef9126103828af637e8f1
pacul890 <pasutrigamblo@gmail.com>
2024-09-30 10:57:54 +0700

    Add files via upload

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
-# Copyright (c) 2013-2016 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+# You need autoconf 2.5x, preferably 2.57 or later
+# You need automake 1.7 or later. 1.6 might work.
 
 set -e
-srcdir="$(dirname $0)"
-cd "$srcdir"
-if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
-  LIBTOOLIZE="${GLIBTOOLIZE}"
-  export LIBTOOLIZE
-fi
-which autoreconf >/dev/null || \
-  (echo "configuration failed, please install autoconf first" && exit 1)
-autoreconf --install --force --warnings=all
+
+aclocal
+autoheader
+automake --gnu --add-missing --copy
+autoconf
+
